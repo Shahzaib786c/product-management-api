5709602dcbbe0f021e16f5e14c47f2205d504030
DELLMUSAMMIL <you@example.com>
2026-08-15 02:06:28 +0500

    Categories crud With Mongodb and postman Testing Done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,8 @@
 import Category from "../models/Category.js";
 
-export async function createCategory(req, res, next) {
+export async function createCategory(req, res) {
     try {
-        const student = await Category.create(req.body);
+        const category = await Category.create(req.body);
         res.status(201).json(
             {
                 message: "Category created Successfully!",
@@ -10,8 +10,92 @@ export async function createCategory(req, res, next) {
         );
     }
     catch (error) {
-        return res.status(400).json({
-            message: error.message || "Failed to create category"
-        });
+        return res.status(400).json(
+            {
+                message: error.message
+            });
+    }
+}
+
+
+export async function getCategories(req, res) {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(
+            {
+                total: categories.length,
+                data: categories
+            });
+    }
+    catch (error) {
+        return res.status(400).json(
+            {
+                message: error.message
+            });
+    }
+}
+
+export async function getCategory(req, res) {
+    try {
+        const category = await Category.findById(
+            req.params.id,
+        );
+        if (!category) {
+            return res.status(404).json(
+                {
+                    message: "Category not found"
+                }
+            );
+        }
+        res.status(200).json(category);
+    } catch (error) {
+        res.status(500).json(
+            {
+                message: error.message
+            });
+    }
+}
+
+export async function updateCategory(req, res) {
+    try {
+        const category = await Category.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                returnDocument: 'after',
+                runValidators: true
+            }
+        );
+        if (!category) {
+            return res.status(404).json(
+                {
+                    message: "Category not found"
+                }
+            );
+        }
+        res.status(200).json(category);
+    } catch (error) {
+        res.status(500).json(
+            {
+                message: error.message
+            });
+    }
+}
+
+export async function deleteCategory(req, res) {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+        if (!category) {
+            return res.status(404).json(
+                {
+                    message: "Category not found"
+                });
+        }
+        res.status(200).json({ message: "Category deleted" });
+    } catch (error) {
+        res.status(500).json(
+            {
+                message: error.message
+            });
     }
 }
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import { createCategory, getCategories, getCategory, updateCategory, deleteCategory } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
-router.post("/",createCategory)
+router.post("/", createCategory)
+router.get("/", getCategories)
+router.get("/:id", getCategory)
+router.put("/:id", updateCategory)
+router.delete("/:id", deleteCategory)
 
 export default router;
